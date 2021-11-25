f56d3632aecdb8f62b28a62051e61b505e14cbc7
sam38124 <sam38124@gmail.com>
2021-11-25 13:21:45 +0800

    32

--- Entry.js
+++ Entry.js
@@ -27,6 +27,38 @@ ${text}
 `
         }
     }
+    glitter.share.naviGation=  [
+        {
+            title: '開始', items: [{
+                title: '關於Glitter', link: '../frag/Frag_About.html'
+            }, {
+                title: '下載與引用', link: '../frag/Frag_Download_Project.html'
+            },
+                {title: '安卓配置', link: '../android/Frag_Sdk_Initial.html'},
+                {title: 'IOS配置', link: '../ios/Frag_Sdk_Initial.html'}]
+        }, {
+            title: '開發組件', items: [
+                {title: '進入點', link: '../Development/Frag_Entry.html'},
+                {title: '頁面的切換', link: '../Development/Frag_Switch_Page.html'},
+                {title: '片段與視窗', link: '../Development/Frag_Frag.html'},
+                {title: '畫面的繪製', link: '../Development/Frag_View_Paint.html'},
+                {title: 'MVVM雙向綁定', link: '../Development/Frag_View_Binding.html'},
+                {title: '資料的傳遞', link: '../Development/Frag_DataTransFer.html'}
+            ]
+        }, {
+            title: '通用插件', items: [
+                {title: '藍芽', link: '../plugins/bluetooth.html'},
+                {title: '資料庫',link: '../plugins/DataBase.html'}
+            ]
+        },{
+            title: 'Android插件', items: [
+                {title: 'Permission', link: '../android/plugins/Permission.html'}
+            ]
+        },{
+            title: 'IOS插件', items: [
+            ]
+        }
+    ]
     glitter.setHome('page/Doc_Index.html', 'Doc_Index', {})
 }
 
